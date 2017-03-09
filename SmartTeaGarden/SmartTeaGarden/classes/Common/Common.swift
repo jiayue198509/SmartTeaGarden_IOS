49355e26c3dc99216d8b0325a0a6121bf1453a22
jiaerdong <jiaerdong@jiaerdongdeMacBook-Pro.local>
2017-03-09 17:01:11 +0800

    添加基本框架

---
+++ SmartTeaGarden/SmartTeaGarden/classes/Common/Common.swift
@@ -0,0 +1,13 @@
+//
+//  Common.swift
+//  SmartTeaGarden
+//
+//  Created by jiaerdong on 2017/3/9.
+//  Copyright © 2017年 tianciqinyun. All rights reserved.
+//
+
+import Foundation
+
+public let kAppId = ""
+public let kAppKey = ""
+
